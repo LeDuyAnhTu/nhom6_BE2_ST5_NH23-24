912817da25441210ab5a018082414a7e226c2555
hthuongg <dtht11102024@gmail.com>
2024-03-26 15:57:46 +0700

    fix template route

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,8 @@ use App\Http\Controllers\Product;
 Route::resource("/product", Product::class);
 
 //Dùng controller
-Route::post('/{name?}', [Signup::class, 'show'] );
-Route::get('/{name?}', [Signup::class, 'show'] );
+// Route::post('/{name?}', [Signup::class, 'show'] );
+// Route::get('/{name?}', [Signup::class, 'show'] );
 
 //Dùng request trực tiếp
 // Route::post('/demo', function(Request $request){
@@ -33,26 +33,31 @@ Route::get('/{name?}', [Signup::class, 'show'] );
 //     return view('demo');
 // })->middleware('checkname');
 
+// Route::group(['as' => 'name.'], function () {
+//     Route::get('/{name?}', function ($name = 'trangchu') {
+//         return view($name);
+//         // ->with('nav', file_get_contents(__DIR__ . "/../resources/views/navbar.blade.php"));
+//     })->name('view');
+// });
 
-Route::group(['as' => 'name.'], function () {
-    Route::get('/{name?}', function ($name = 'trangchu') {
-        return view($name);
-        // ->with('nav', file_get_contents(__DIR__ . "/../resources/views/navbar.blade.php"));
-    })->name('view');
-});
-
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('template.index', function () {
-    return view('template.index');
-});
-
-Route::get('/xinchao/{name?}',function($name="22211TT0024") {
-    return view('demo')->with('name',$name);
-})->where('name','[0-9]{2}[2-5]11[A-Z]{2}[0-9]{4}');
+// Route::get('/xinchao/{name?}',function($name="22211TT0024") {
+//     return view('demo')->with('name',$name);
+// })->where('name','[0-9]{2}[2-5]11[A-Z]{2}[0-9]{4}');
 
 
 // Route::get('/{name?}',function($name="22211TT0024") {
 //     return view('demo')->with('name',$name);
 // });
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', function () {
+    return view('template.index');
+});
+
+Route::group(['as' => 'name'], function () {
+    Route::get('/{name?}', function ($name = 'index') {
+        return view('template.'.$name);
+    });
+});
